29539bbdf308f7edc524caee6b904e7303c379fd
HeiLL09 <110545905+HeiLL09@users.noreply.github.com>
2025-05-01 14:41:45 +0000

    new file:   960.webp

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import time
+backgroundColor= "#000000"
+on = st.toggle("Sigma")
+if on:
+  st.image("960.webp")
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.write("Smurfik Kombat")
+left, middle, right = st.columns(3)
+
+
+if left.button("Кликнуть", type = "primary"):
+   left.markdown("Ты же не думал, что я что то сделал?")
