7ab645c6c5ac34455956b1f9f1f62223fbff23dd
HeiLL09 <110545905+HeiLL09@users.noreply.github.com>
2025-05-03 11:15:51 +0000

    new file: document_5436337962805651912.mp4 new file: SmurfikKombat.py new file: pages/1_Casino.py new file: pages/2_Sigma.py new file: pages/3_New.py delete file: streamlit_app.py

---
+++ SmurfikKombat.py
@@ -0,0 +1,26 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Smurfik",
+    page_icon="😈",
+)
+backgroundColor= "#000000"
+st.write("# Это приложение смурфик")
+
+st.markdown("Это основная страница моего приложения")
+
+
+
+# streamlit run SmurfikKombat.py
+
+
+
+
+
+
+
+
+
+
+
+
+
